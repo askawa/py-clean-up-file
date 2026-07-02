1ac7abeb1e53120c0fdfaeabf95db6c040900f3d
askawa <ask_awa@icloud.com>
2026-07-02 12:26:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+import os
+from pytest import File
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> File:
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> bool:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+        return False
